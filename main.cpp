1fc76c96685a716d37173884cc00edeb42d25fbf
K023g <K023g@MSI>
2024-04-17 17:06:20 +0900

    デバイスの設定

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
@@ -16,9 +25,42 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
 // Windowsアプリのエントリーぽイント
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+
 	WNDCLASS wc{};
 	//ウィンドウプロシージャ
 	wc.lpfnWndProc = WindowProc;
@@ -56,8 +98,73 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		nullptr					//メニューウハンドル
 		);
 
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
+	//HRESULはWindows系のエラーコードあり
+	//関数が成功したかどうかをSUCCEEDEマクロで判定できる
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
+	//曽木課の根本な部分でエラーが出た場合が多いのでassertにしておく
+	assert(SUCCEEDED(hr));
+
+
+	//使用するアダプタ用の変数。最初にnullpterを入れておく
+	IDXGIAdapter4* UseAdapter = nullptr;
+	//いい順にアダプターを頼む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&UseAdapter))
+		!= DXGI_ERROR_NOT_FOUND; ++i)
+	{
+		//アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = UseAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));
+		//ソフトウェアアダプタでなければ採用
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)){
+			//採用したアダプタの情報をログに出力
+			Log( ConvertString( std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		UseAdapter = nullptr;//ソフトウェアアダプタの場合は見なかったことにする
+	}
+	//適切なアダプターが見つからなかったので起動できない
+	assert(UseAdapter != nullptr);
+
+	ID3D12Device* device = nullptr;
+	//昨日レベルとログ出力用の文字列
+	D3D_FEATURE_LEVEL fealtureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1, D3D_FEATURE_LEVEL_12_0
+	};
+	const char* featureLevelString[] = { "12.2", "12.1" , "12.0" };
+	//高い順に生成できるか試していく
+	for (size_t i = 0; i < _countof(fealtureLevels); ++i)
+	{
+		//採用したアダプターでデバイスを生成
+		hr = D3D12CreateDevice(UseAdapter, fealtureLevels[i], IID_PPV_ARGS(&device));
+		//指定した機能レベルでデバイス生成
+		if (SUCCEEDED(hr)) {
+			//生成できたのでログ出力を行ってループを抜ける
+			Log(std::format("Featurrelevel : {} \n", featureLevelString[i]));
+			break;
+		}
+	}
+	//デバイスの生成がうまくいかなかったので起動できない
+	assert(device != nullptr);
+	Log("Compleate create DeD12DeVICE!!!\n");//初期化完了のログを出す
+
 	//ウィンドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+	//ウィンドウの×ボタンが押されるまでループ
+	while (msg.message != WM_QUIT) {
+		//Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			//ゲームの処理
+		}
+	}
 
 	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello.DirectX!\n");
