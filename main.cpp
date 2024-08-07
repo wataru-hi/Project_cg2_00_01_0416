3f3f976ab6645172c2eb861a6cea69aa5b1dc727
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2024-08-07 15:51:59 +0900

    a

--- main.cpp
+++ main.cpp
@@ -553,78 +553,76 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 
 	//使用するアダプタ用の変数。最初にnullpterを入れておく
-	IDXGIAdapter4* UseAdapter = nullptr;
-	//いい順にアダプターを頼む
-	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&UseAdapter))
-		!= DXGI_ERROR_NOT_FOUND; ++i)
-	{
-		//アダプターの情報を取得する
-		DXGI_ADAPTER_DESC3 adapterDesc{};
-		hr = UseAdapter->GetDesc3(&adapterDesc);
-		assert(SUCCEEDED(hr));
-		//ソフトウェアアダプタでなければ採用
-		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
-			//採用したアダプタの情報をログに出力
-			Log(ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
-			break;
-		}
-		UseAdapter = nullptr;//ソフトウェアアダプタの場合は見なかったことにする
+Microsoft::WRL::ComPtr<IDXGIAdapter4> UseAdapter;
+//いい順にアダプターを頼む
+for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&UseAdapter))
+	!= DXGI_ERROR_NOT_FOUND; ++i)
+{
+	//アダプターの情報を取得する
+	DXGI_ADAPTER_DESC3 adapterDesc{};
+	hr = UseAdapter->GetDesc3(&adapterDesc);
+	assert(SUCCEEDED(hr));
+	//ソフトウェアアダプタでなければ採用
+	if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+		//採用したアダプタの情報をログに出力
+		Log(ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
+		break;
 	}
-	//適切なアダプターが見つからなかったので起動できない
-	assert(UseAdapter != nullptr);
+	UseAdapter.Reset();
+}
+//適切なアダプターが見つからなかったので起動できない
+assert(UseAdapter != nullptr);
 
-	ID3D12Device* device = nullptr;
-	//昨日レベルとログ出力用の文字列
-	D3D_FEATURE_LEVEL fealtureLevels[] = {
-		D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1, D3D_FEATURE_LEVEL_12_0
-	};
-	const char* featureLevelString[] = { "12.2", "12.1" , "12.0" };
-	//高い順に生成できるか試していく
-	for (size_t i = 0; i < _countof(fealtureLevels); ++i)
-	{
-		//採用したアダプターでデバイスを生成
-		hr = D3D12CreateDevice(UseAdapter, fealtureLevels[i], IID_PPV_ARGS(&device));
-		//指定した機能レベルでデバイス生成
-		if (SUCCEEDED(hr)) {
-			//生成できたのでログ出力を行ってループを抜ける
-			Log(std::format("Featurrelevel : {} \n", featureLevelString[i]));
-			break;
-		}
+Microsoft::WRL::ComPtr<ID3D12Device> device;
+//昨日レベルとログ出力用の文字列
+D3D_FEATURE_LEVEL fealtureLevels[] = {
+	D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1, D3D_FEATURE_LEVEL_12_0
+};
+const char* featureLevelString[] = { "12.2", "12.1" , "12.0" };
+//高い順に生成できるか試していく
+for (size_t i = 0; i < _countof(fealtureLevels); ++i)
+{
+	//採用したアダプターでデバイスを生成
+	hr = D3D12CreateDevice(UseAdapter.Get(), fealtureLevels[i], IID_PPV_ARGS(&device));
+	//指定した機能レベルでデバイス生成
+	if (SUCCEEDED(hr)) {
+		//生成できたのでログ出力を行ってループを抜ける
+		Log(std::format("Featurrelevel : {} \n", featureLevelString[i]));
+		break;
 	}
-	//デバイスの生成がうまくいかなかったので起動できない
-	assert(device != nullptr);
-	Log("Compleate create DeD12Device!!!\n");//初期化完了のログを出す
+}
+//デバイスの生成がうまくいかなかったので起動できない
+assert(device != nullptr);
+Log("Compleate create DeD12Device!!!\n");//初期化完了のログを出す
 
 #ifdef _DEBUG
-	ID3D12InfoQueue* InfoQueue = nullptr;
-	if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&InfoQueue))))
-	{
-		//ヤバイエラー時に止まる
-		InfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
-		//エラー時に止まる
-		InfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
-		//警告時に止まる
-		InfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+Microsoft::WRL::ComPtr<ID3D12InfoQueue> InfoQueue;
+if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&InfoQueue))))
+{
+	//ヤバイエラー時に止まる
+	InfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+	//エラー時に止まる
+	InfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+	//警告時に止まる
+	InfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
 
-		//抑制するメッセージID
-		D3D12_MESSAGE_ID denyIds[] = {
-			//
-			//
-			D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE
-		};
-		//抑圧するレベル
-		D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
-		D3D12_INFO_QUEUE_FILTER filter{};
-		filter.DenyList.NumIDs = _countof(denyIds);
-		filter.DenyList.pIDList; denyIds;
-		filter.DenyList.NumSeverities = _countof(severities);
-		filter.DenyList.pSeverityList = severities;
-		//指定したメッセージの表示を抑制する
-		InfoQueue->PushStorageFilter(&filter);
+	//抑制するメッセージID
+	D3D12_MESSAGE_ID denyIds[] = {
+		//
+		//
+		D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE
+	};
+	//抑圧するレベル
+	D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
+	D3D12_INFO_QUEUE_FILTER filter{};
+	filter.DenyList.NumIDs = _countof(denyIds);
+	filter.DenyList.pIDList; denyIds;
+	filter.DenyList.NumSeverities = _countof(severities);
+	filter.DenyList.pSeverityList = severities;
+	//指定したメッセージの表示を抑制する
+	InfoQueue->PushStorageFilter(&filter);
 
-		//解放
-		InfoQueue->Release();
-	}
+}
 #endif
 
 	//コマンドキューを生成する
