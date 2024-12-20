a418949a64a24484fd3222a51ff01a9cf53177a9
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2024-12-20 13:46:00 +0900

    Revert "l"
    This reverts commit c2c7e2c71435f9b6d760b90df86b5574688401d1.

--- Resources/shader/Object3d.PS.hlsl
+++ Resources/shader/Object3d.PS.hlsl
@@ -12,9 +12,17 @@ struct PixcelShaderOutput
     float32_t4 color : SV_Target0;
 };
 
+struct DirectrionaLight
+{
+    float32_t4 color; //!< ライトの色
+    float32_t3 direction; //!< ライトの向き
+    float intensity;
+};
+
 ConstantBuffer<Material> gMaterial : register(b0);
 Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
+ConstantBuffer<DirectrionaLight> gDirectrionaLight : register(b1);
 
 PixcelShaderOutput main(VertexShaderOutput input)
 {   
@@ -23,7 +31,7 @@ PixcelShaderOutput main(VertexShaderOutput input)
     float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     
-    if (gMaterial.enableLightng != 0)//Liting����ꍇ
+    if (gMaterial.enableLightng != 0)//Litingする場合
     {
         float NdotL = dot(normalize(input.normal), -gDirectrionaLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
@@ -33,6 +41,7 @@ PixcelShaderOutput main(VertexShaderOutput input)
     {
         output.color = gMaterial.color * textureColor;
     }
+    
     
     return output;
 }
