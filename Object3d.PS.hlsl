287bd9469a43d0da152f2e55c5147a72700f8d0d
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2025-02-13 13:08:26 +0900

    PSのshininessでエラー

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -5,7 +5,7 @@ struct Material
     float32_t4 color;
     int32_t enableLightng;
     float32_t shininess;
-    };
+};
 
 struct PixcelShaderOutput
 {
@@ -18,10 +18,10 @@ struct DirectrionaLight
     float32_t3 direction; //!< ライトの向き
     float intensity;
 };
-    
+
 struct Camera
 {
-    float32_t worldPosition;
+    float32_t3 worldPosition; // カメラの位置をfloat3で定義
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
@@ -31,40 +31,40 @@ ConstantBuffer<DirectrionaLight> gDirectrionaLight : register(b1);
 ConstantBuffer<Camera> gCamera : register(b2);
 
 PixcelShaderOutput main(VertexShaderOutput input)
-{   
+{
     PixcelShaderOutput output;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    float32_t3 toEye = normalize(gCamera.worldPosition - input.wotldPosition);
-    float32_t3 reflectLight = reflect(gDirectrionaLight.direction, normalize(input.normal));
-        
+    float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition); // カメラ位置を使用
+    float32_t3 reflectLight = reflect(-gDirectrionaLight.direction, normalize(input.normal)); // 光源方向を反転
+    float NdotL = dot(normalize(input.normal), -gDirectrionaLight.direction); // 追加
+
     float RdotE = dot(reflectLight, toEye);
-    float specularPow = pow(saturate(RdotE), gMaterial.shininess);
-        
-    //拡散反射
-    float32_t3 diffuse =
-    gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+    //float specularPow = pow(saturate(RdotE), gMaterial.shininess);
+    float specularPow = pow(saturate(RdotE), 70);
 
-    // 鏡面反射
-    float32_t3 specular =
-    gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
-
-    //拡散反射+鏡面反射
-    output.color.rgb = diffuse + specular; // 物体の鏡面反射色。ここでは白にしている materialで設定できたりすると良い
-
-// アルファは今まで通り
-    output.color.a = gMaterial.color.a * textureColor.a;
-        
     if (gMaterial.enableLightng != 0)//Litingする場合
     {
-        float NdotL = dot(normalize(input.normal), -gDirectrionaLight.direction);
-        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-        output.color = gMaterial.color * textureColor * gDirectrionaLight.color * cos * gDirectrionaLight.intensity;
+        float cos = saturate(NdotL); // NdotLをsaturateで0～1の範囲にクランプ
+
+        // 拡散反射
+        float32_t3 diffuse =
+            gMaterial.color.rgb * textureColor.rgb * gDirectrionaLight.color.rgb * cos * gDirectrionaLight.intensity;
+
+        // 鏡面反射
+        float32_t3 specular =
+            gDirectrionaLight.color.rgb * gDirectrionaLight.intensity * specularPow;
+
+        // 拡散反射+鏡面反射
+        output.color.rgb = diffuse + specular; // 物体の鏡面反射色。ここでは白にしている materialで設定できたりすると良い
+
+        // アルファは今まで通り
+        output.color.a = gMaterial.color.a * textureColor.a;
     }
     else
     {
         output.color = gMaterial.color * textureColor;
+        output.color.a = gMaterial.color.a * textureColor.a; // elseの場合もアルファを設定
     }
-    
-    
+
     return output;
 }
--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -8,18 +8,19 @@ struct TransformationMatrix
 
 ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
-struct VertexShederInput {
+struct VertexShaderInput
+{
     float32_t4 position : POSITION0;
-    float32_t2 teccoord : TEXCOORD0;
+    float32_t2 texcoord : TEXCOORD0;
     float32_t3 normal : NORMAL0;
 };
 
-VertexShaderOutput main(VertexShederInput input)
+VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
     output.position = mul(input.position, gTransformationMatrix.WVP);
-    output.texcoord = input.teccoord;
+    output.texcoord = input.texcoord;
     output.normal = normalize(mul(input.normal, (float32_t3x3)gTransformationMatrix.World));
-    output.wotldPosition = mul(input.position, gTransformationMatrix.World).xyz);
+    output.worldPosition = mul(input.position, gTransformationMatrix.World).xyz;
     return output;
 }
